21d0c9d5744de5916ca03f6b6e1ef87c323a0184
Angel <conglongyu@vip.qq.com>
2017-01-20 14:00:04 +0800

    bottom-navigation

---
+++ app/src/main/java/com/longcai/conveniencenet/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.longcai.conveniencenet;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 丛龙宇 on 17-1-20.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/longcai/conveniencenet/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.longcai.conveniencenet;
+
+import android.app.Fragment;
+
+/**
+ * Created by 丛龙宇 on 17-1-20.
+ */
+
+public class BaseFragment extends Fragment {
+}
